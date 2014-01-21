73986cb5f72e760e4e7831e0c1407473afb47b36
Dmitry Yakimenko <detunized@gmail.com>
2014-01-22 00:05:07 +0100

    Curl web client

---
+++ src/curlwebclient.cpp
@@ -0,0 +1,18 @@
+#include "curlwebclient.h"
+
+#include <curl/curl.h>
+
+namespace lastpass
+{
+
+std::string CurlWebClient::get(std::string const &url, std::map<std::string, std::string> const &values)
+{
+    return {};
+}
+
+std::string CurlWebClient::post(std::string const &url, std::map<std::string, std::string> const &values)
+{
+    return {};
+}
+
+}
---
+++ src/curlwebclient.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "webclient.h"
+
+namespace lastpass
+{
+
+class CurlWebClient: public WebClient
+{
+public:
+    virtual std::string get(std::string const &url, std::map<std::string, std::string> const &values) override;
+    virtual std::string post(std::string const &url, std::map<std::string, std::string> const &values) override;
+};
+
+}
