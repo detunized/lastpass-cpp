99a9adfbb2ad688f75dd2a49057d523b37792780
Dmitry Yakimenko <detunized@gmail.com>
2014-01-22 00:28:03 +0100

    Global cURL intializer

--- src/curlwebclient.cpp
+++ src/curlwebclient.cpp
@@ -5,6 +5,25 @@
 namespace lastpass
 {
 
+namespace
+{
+
+class CurlGlobalInitializer
+{
+public:
+    CurlGlobalInitializer()
+    {
+        curl_global_init(CURL_GLOBAL_ALL);
+    }
+
+    ~CurlGlobalInitializer()
+    {
+        curl_global_cleanup();
+    }
+} curl_global_initializer;
+
+}
+
 std::string CurlWebClient::get(std::string const &url, std::map<std::string, std::string> const &values)
 {
     return {};
