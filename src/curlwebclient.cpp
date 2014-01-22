0b407d0b572707af4713f1145c80dcbe3e7660af
Dmitry Yakimenko <detunized@gmail.com>
2014-01-22 01:03:43 +0100

    GET request via cURL

--- src/curlwebclient.cpp
+++ src/curlwebclient.cpp
@@ -25,10 +25,12 @@ public:
 class Curl
 {
 public:
-    Curl(): curl_(curl_easy_init())
+    explicit Curl(std::string const &url): curl_(curl_easy_init())
     {
         if (curl_ == nullptr)
             throw std::runtime_error("Failed to initialize cURL");
+
+        curl_easy_setopt(curl_, CURLOPT_URL, url.c_str());
     }
 
     ~Curl()
@@ -49,11 +51,28 @@ private:
     Curl &operator =(Curl const &) = delete;
 };
 
+size_t store_response(void *buffer, size_t size, size_t count, std::string *response_out)
+{
+    auto total = size * count;
+    response_out->append((char const *)buffer, total);
+    return total;
+}
+
 }
 
 std::string CurlWebClient::get(std::string const &url, std::map<std::string, std::string> const &values)
 {
-    return {};
+    Curl curl(url);
+
+    std::string response;
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &store_response);
+
+    CURLcode result = curl_easy_perform(curl);
+    if (result != CURLE_OK)
+        throw std::runtime_error("GET failed");
+
+    return response;
 }
 
 std::string CurlWebClient::post(std::string const &url, std::map<std::string, std::string> const &values)
