2f072babc51a5f1bee9c0e045e62844709a4ec90
Dmitry Yakimenko <detunized@gmail.com>
2014-02-06 10:08:01 +0100

    AtExit cleanup class for C objects

--- src/utils.h
+++ src/utils.h
@@ -1,9 +1,22 @@
 #pragma once
 
+#include <functional>
 #include <string>
 
 namespace lastpass
 {
+
+class AtExit
+{
+public:
+    typedef std::function<void()> Function;
+
+    AtExit(Function &&f): f_(f) {}
+    ~AtExit() { f_(); }
+
+private:
+    Function f_;
+};
 
 std::string to_hex(std::string const &bytes);
 std::string decode_base64(std::string const &base64_text);
