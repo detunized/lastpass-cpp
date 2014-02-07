71b92fddf4b1ebe7d6825c83914166b6c8d7ed40
Dmitry Yakimenko <detunized@gmail.com>
2014-02-07 02:24:49 +0100

    Print vault from the example

--- example/example.cpp
+++ example/example.cpp
@@ -1,5 +1,4 @@
-#include "../src/curlwebclient.h"
-#include "../src/fetcher.h"
+#include "../src/vault.h"
 
 #include <iostream>
 
@@ -9,16 +8,17 @@ int main(int argc, char const *argv[])
 
     try
     {
-        CurlWebClient web_client;
-        std::cout << "GET:\n"
-                  << web_client.get("http://httpbin.org/get", {{"first name", "Milton"}, {"last name", "Waddams"}})
-                  << "\n";
-        std::cout << "POST:\n"
-                  << web_client.post("http://httpbin.org/post", {{"first name", "Bill"}, {"last name", "Lumbergh"}})
-                  << "\n";
-        std::cout << "GET:\n"
-                  << web_client.get("http://httpbin.org/cookies", {{}}, {{"bob 1", "Bob Slydell"}, {"bob 2", "Bob Porter"}})
-                  << "\n";
+        auto vault = Vault::create("username", "password");
+        for (auto const &i: vault.accounts())
+        {
+            std::cout << "      id: " << i.id() << '\n'
+                      << "    name: " << i.name() << '\n'
+                      << "username: " << i.username() << '\n'
+                      << "password: " << i.password() << '\n'
+                      << "   group: " << i.group() << '\n'
+                      << "     url: " << i.url() << '\n'
+                      << '\n';
+        }
     }
     catch (std::exception const &e)
     {
