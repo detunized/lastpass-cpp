62c20168c3ec44abdd305ad54a22148d100a3c16
Dmitry Yakimenko <detunized@gmail.com>
2014-02-08 12:37:39 +0100

    Move instead of copying (closes #2)

--- src/utils.h
+++ src/utils.h
@@ -11,8 +11,15 @@ class AtExit
 public:
     typedef std::function<void()> Function;
 
-    explicit AtExit(Function &&f): f_(f) {}
-    ~AtExit() { f_(); }
+    explicit AtExit(Function &&f):
+        f_(std::move(f))
+    {
+    }
+
+    ~AtExit()
+    {
+        f_();
+    }
 
 private:
     Function f_;
