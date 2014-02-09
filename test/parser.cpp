acf4f0736f75dd26c7addb5b79aebf80397d8ad9
Dmitry Yakimenko <detunized@gmail.com>
2014-02-09 13:34:39 +0100

    Parser tests moved out

---
+++ test/parser.cpp
@@ -0,0 +1,107 @@
+#include "test.h"
+
+namespace
+{
+
+using namespace lastpass;
+using namespace test;
+
+auto const BLOB_BYTES = "\x4c\x50\x41\x56\x00\x00\x00\x03\x31\x31\x38"_s;
+auto const ENCRYPTION_KEY = "\x39\xf3\x94\xbd\x59\xd0\xcc\x1e\x2f\xe3\xdb\x0d\x87\x8f\x8f\x77"
+                            "\x02\x05\x6f\xd1\x6b\xe7\xe8\xd5\x7d\x64\x53\x7f\xe1\x36\x1a\x18"_s;
+
+BOOST_AUTO_TEST_CASE(parser_extract_chunks)
+{
+    std::istringstream s(std::string(std::begin(BLOB_BYTES), std::end(BLOB_BYTES)));
+    BOOST_CHECK(Parser::extract_chunks(s)
+                == (Chunks {{ChunkId::LPAV, {"\x31\x31\x38"_s}}}));
+}
+
+BOOST_AUTO_TEST_CASE(parser_extract_chunks_with_filter)
+{
+    std::istringstream s(std::string(std::begin(BLOB_BYTES), std::end(BLOB_BYTES)));
+    BOOST_CHECK(Parser::extract_chunks(s, {ChunkId::LPAV})
+                == (Chunks {{ChunkId::LPAV, {"\x31\x31\x38"_s}}}));
+}
+
+BOOST_AUTO_TEST_CASE(parser_extract_chunks_accounts)
+{
+    std::istringstream s(data::BLOB);
+    auto chunks = Parser::extract_chunks(s, {ChunkId::ACCT});
+
+    BOOST_CHECK_EQUAL(chunks.size(), 1);
+    BOOST_CHECK_EQUAL(chunks[ChunkId::ACCT].size(), data::ACCOUNTS.size());
+
+    auto const &accounts = chunks[ChunkId::ACCT];
+    for (size_t i = 0, size = accounts.size(); i < size; ++i)
+        check_equal(Parser::parse_account(accounts[i], data::ENCRYPTION_KEY), data::ACCOUNTS[i]);
+}
+
+BOOST_AUTO_TEST_CASE(parser_decrypt_aes256_ecb_plain)
+{
+    std::map<std::string, std::string> const test_cases
+    {
+        {"", ""},
+        {"0123456789", "\xf2\x61\xf1\x20\x0f\x2b\xba\x5e"
+                       "\x9e\xab\xbd\x9a\xfc\x6a\xb6\x8b"_s},
+        {"All your base are belong to us", "\x04\xd8\x5d\xdd\x0d\xd9\x54\xe0"
+                                           "\xf1\x93\xd7\x34\x0b\xbf\x3c\x35"
+                                           "\x43\xd3\x21\xf6\x27\x66\xec\x57"
+                                           "\x7e\x48\x21\xb4\xc2\x7c\x8d\x53"_s},
+    };
+
+    for (auto const &i: test_cases)
+        BOOST_CHECK_EQUAL(Parser::decrypt_aes256_ecb_plain(i.second, ENCRYPTION_KEY), i.first);
+}
+
+BOOST_AUTO_TEST_CASE(parser_decrypt_aes256_ecb_base64)
+{
+    std::map<std::string, std::string> const test_cases
+    {
+        {"", ""},
+        {"0123456789", "8mHxIA8rul6eq72a/Gq2iw=="},
+        {"All your base are belong to us", "BNhd3Q3ZVODxk9c0C788NUPTIfYnZuxXfkghtMJ8jVM="},
+    };
+
+    for (auto const &i: test_cases)
+        BOOST_CHECK_EQUAL(Parser::decrypt_aes256_ecb_base64(i.second, ENCRYPTION_KEY), i.first);
+}
+
+BOOST_AUTO_TEST_CASE(parser_decrypt_aes256_cbc_plain)
+{
+    std::map<std::string, std::string> const test_cases
+    {
+        {
+            "0123456789",
+
+            "\x21\x0f\xa1\x88\x8c\xb4\xbc\x61\xb8\xb2\xbb\x07\x99\x70\xa1\x7b"
+            "\x77\xa1\x59\xcf\xeb\x60\xf9\xdf\x8b\x2a\x8e\x1b\xc8\x7b\xf0\x37\x57"_s
+        },
+        {
+            "All your base are belong to us",
+
+            "\x21\xca\x24\x0d\x69\xa3\x3a\x42\xad\x2e\x96\x5e\x85\x62\x8b\xeb"
+            "\xae\xba\x52\x3e\x9f\x35\x73\xe9\x5f\xa9\x4b\x5a\x5a\x2e\xfb\x52"
+            "\xeb\xc1\xbf\x83\xdf\x29\x8f\xe8\x10\x30\x77\xa0\xb4\x4c\x1d\xe1\x48"_s
+        }
+    };
+
+    for (auto const &i: test_cases)
+        BOOST_CHECK_EQUAL(Parser::decrypt_aes256_cbc_plain(i.second, ENCRYPTION_KEY), i.first);
+}
+
+BOOST_AUTO_TEST_CASE(parser_decrypt_aes256_cbc_base64)
+{
+    std::map<std::string, std::string> const test_cases
+    {
+        {"0123456789",
+         "!6TZb9bbrqpocMaNgFjrhjw==|f7RcJ7UowesqGk+um+P5ug=="},
+        {"All your base are belong to us",
+         "!YFuiAVZgOD2K+s6y8yaMOw==|TZ1+if9ofqRKTatyUaOnfudletslMJ/RZyUwJuR/+aI="},
+    };
+
+    for (auto const &i: test_cases)
+        BOOST_CHECK_EQUAL(Parser::decrypt_aes256_cbc_base64(i.second, ENCRYPTION_KEY), i.first);
+}
+
+}
--- test/test.cpp
+++ test/test.cpp
@@ -1,33 +1,11 @@
 // TODO: Split this module into separate files!
 
+// This defines main.
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE lastpass
+#include <boost/test/unit_test.hpp>
 
 #include "test.h"
-
-#include "../src/account.h"
-#include "../src/crypto.h"
-#include "../src/fetcher.h"
-#include "../src/parser.h"
-#include "../src/session.h"
-#include "../src/vault.h"
-#include "../src/utils.h"
-
-#define C(dd) (static_cast<char>(0x##dd))
-
-using namespace lastpass;
-using namespace test;
-
-namespace
-{
-
-std::string const BLOB_BYTES {C(4c), C(50), C(41), C(56), C(00), C(00), C(00), C(03), C(31), C(31), C(38)};
-std::string const ENCRYPTION_KEY {C(39), C(f3), C(94), C(bd), C(59), C(d0), C(cc), C(1e),
-                                  C(2f), C(e3), C(db), C(0d), C(87), C(8f), C(8f), C(77),
-                                  C(02), C(05), C(6f), C(d1), C(6b), C(e7), C(e8), C(d5),
-                                  C(7d), C(64), C(53), C(7f), C(e1), C(36), C(1a), C(18)};
-
-}
 
 namespace lastpass
 {
@@ -47,100 +25,4 @@ void check_equal(Account const &account, data::Account const &expected)
 
 }
 
-}
-
-BOOST_AUTO_TEST_CASE(Parser_extract_chunks)
-{
-    std::istringstream s(std::string(std::begin(BLOB_BYTES), std::end(BLOB_BYTES)));
-    BOOST_CHECK(Parser::extract_chunks(s) == (Chunks {{ChunkId::LPAV, {{C(31), C(31), C(38)}}}}));
-}
-
-BOOST_AUTO_TEST_CASE(Parser_extract_chunks_with_filter)
-{
-    std::istringstream s(std::string(std::begin(BLOB_BYTES), std::end(BLOB_BYTES)));
-    BOOST_CHECK(Parser::extract_chunks(s, {ChunkId::LPAV}) == (Chunks {{ChunkId::LPAV, {{C(31), C(31), C(38)}}}}));
-}
-
-BOOST_AUTO_TEST_CASE(Parser_extract_chunks_accounts)
-{
-    std::istringstream s(data::BLOB);
-    auto chunks = Parser::extract_chunks(s, {ChunkId::ACCT});
-    BOOST_CHECK_EQUAL(chunks.size(), 1);
-    BOOST_CHECK_EQUAL(chunks[ChunkId::ACCT].size(), data::ACCOUNTS.size());
-
-    auto const &accounts = chunks[ChunkId::ACCT];
-    for (size_t i = 0, size = accounts.size(); i < size; ++i)
-        check_equal(Parser::parse_account(accounts[i], data::ENCRYPTION_KEY), data::ACCOUNTS[i]);
-}
-
-BOOST_AUTO_TEST_CASE(Parser_decrypt_aes256_ecb_plain)
-{
-    std::map<std::string, std::string> const test_cases
-    {
-        {"", ""},
-        {"0123456789", {C(f2), C(61), C(f1), C(20), C(0f), C(2b), C(ba), C(5e),
-                        C(9e), C(ab), C(bd), C(9a), C(fc), C(6a), C(b6), C(8b)}},
-        {"All your base are belong to us", {C(04), C(d8), C(5d), C(dd), C(0d), C(d9), C(54), C(e0),
-                                            C(f1), C(93), C(d7), C(34), C(0b), C(bf), C(3c), C(35),
-                                            C(43), C(d3), C(21), C(f6), C(27), C(66), C(ec), C(57),
-                                            C(7e), C(48), C(21), C(b4), C(c2), C(7c), C(8d), C(53)}},
-    };
-
-    for (auto const &i: test_cases)
-        BOOST_CHECK_EQUAL(Parser::decrypt_aes256_ecb_plain(i.second, ENCRYPTION_KEY), i.first);
-}
-
-BOOST_AUTO_TEST_CASE(Parser_decrypt_aes256_ecb_base64)
-{
-    std::map<std::string, std::string> const test_cases
-    {
-        {"", ""},
-        {"0123456789", "8mHxIA8rul6eq72a/Gq2iw=="},
-        {"All your base are belong to us", "BNhd3Q3ZVODxk9c0C788NUPTIfYnZuxXfkghtMJ8jVM="},
-    };
-
-    for (auto const &i: test_cases)
-        BOOST_CHECK_EQUAL(Parser::decrypt_aes256_ecb_base64(i.second, ENCRYPTION_KEY), i.first);
-}
-
-BOOST_AUTO_TEST_CASE(Parser_decrypt_aes256_cbc_plain)
-{
-    std::map<std::string, std::string> const test_cases
-    {
-        {
-            "0123456789",
-            {C(21), C(0f), C(a1), C(88), C(8c), C(b4), C(bc), C(61),
-             C(b8), C(b2), C(bb), C(07), C(99), C(70), C(a1), C(7b),
-             C(77), C(a1), C(59), C(cf), C(eb), C(60), C(f9), C(df),
-             C(8b), C(2a), C(8e), C(1b), C(c8), C(7b), C(f0), C(37),
-             C(57)}
-        },
-        {
-            "All your base are belong to us",
-            {C(21), C(ca), C(24), C(0d), C(69), C(a3), C(3a), C(42),
-             C(ad), C(2e), C(96), C(5e), C(85), C(62), C(8b), C(eb),
-             C(ae), C(ba), C(52), C(3e), C(9f), C(35), C(73), C(e9),
-             C(5f), C(a9), C(4b), C(5a), C(5a), C(2e), C(fb), C(52),
-             C(eb), C(c1), C(bf), C(83), C(df), C(29), C(8f), C(e8),
-             C(10), C(30), C(77), C(a0), C(b4), C(4c), C(1d), C(e1),
-             C(48)}
-        }
-    };
-
-    for (auto const &i: test_cases)
-        BOOST_CHECK_EQUAL(Parser::decrypt_aes256_cbc_plain(i.second, ENCRYPTION_KEY), i.first);
-}
-
-BOOST_AUTO_TEST_CASE(Parser_decrypt_aes256_cbc_base64)
-{
-    std::map<std::string, std::string> const test_cases
-    {
-        {"0123456789",
-         "!6TZb9bbrqpocMaNgFjrhjw==|f7RcJ7UowesqGk+um+P5ug=="},
-        {"All your base are belong to us",
-         "!YFuiAVZgOD2K+s6y8yaMOw==|TZ1+if9ofqRKTatyUaOnfudletslMJ/RZyUwJuR/+aI="},
-    };
-
-    for (auto const &i: test_cases)
-        BOOST_CHECK_EQUAL(Parser::decrypt_aes256_cbc_base64(i.second, ENCRYPTION_KEY), i.first);
 }
